4db4a27e3a6e311edcb5b46bec1d9865a759f2fe
MarsCampbell <johnaj22@hotmail.com>
2018-07-29 23:31:47 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-   // on click pokemon are moved to defenderArea if there is already a pokemon in playerArea
+ // on click pokemon are moved to defenderArea if there is already a pokemon in playerArea
   
  
  $(document).ready(function() {
@@ -80,17 +80,17 @@ $(document).ready(function() {
   var mewHp = 1000;
 
   //atttack
-  var squirtAtk = 10;
-  var charAtk = 30;
-  var bulbAtk = 15;
-  var marioAtk = 20;
+  var squirtAtk = 25;
+  var charAtk = 40;
+  var bulbAtk = 5;
+  var marioAtk = 30;
   
   //counter
-  var squirtCounter = 15;
-  var charCounter = 20;
-  var bulbCounter = 8;
-  var marioCounter = 10;
-  var mewCounter = 100;
+  var squirtCounter = 5;
+  var charCounter = 30;
+  var bulbCounter = 6;
+  var marioCounter = 15;
+  var mewCounter = 50;
 
 
   // is pokemon in playerArea?
@@ -111,12 +111,12 @@ $(document).ready(function() {
   var y = 1;
   var z = 1;
 
+  var q = 0;
+
   var a = 0;
   var b = 0;
   var c = 0;
   var d = 0;
-
-  
 
   // writing pokemon health to inner html
   
@@ -133,112 +133,112 @@ $( "#attackBtn" ).click(function() {
 if (squirtPlay == true && charDef == true) {
  document.getElementById("charmanderHp").innerHTML = charHp - squirtAtk;
  charHp = charHp - squirtAtk;
- x++; squirtAtk = squirtAtk * x;
+ x++; squirtAtk = 25 * x;
  document.getElementById("squirtleHp").innerHTML = squirtHp - charCounter;
  squirtHp = squirtHp - charCounter;
 }
 else if (squirtPlay == true && bulbDef == true) {
  document.getElementById("bulbasaurHp").innerHTML = bulbHp - squirtAtk;
  bulbHp = bulbHp - squirtAtk;
- x++; squirtAtk = squirtAtk * x;
+ x++; squirtAtk = 25 * x;
  document.getElementById("squirtleHp").innerHTML = squirtHp - bulbCounter;
  squirtHp = squirtHp - bulbCounter;
 }
 else if (squirtPlay == true && marioDef == true) {
  document.getElementById("marioHp").innerHTML = marioHp - squirtAtk;
  marioHp = marioHp - squirtAtk;
- x++; squirtAtk = squirtAtk * x;
+ x++; squirtAtk = 25 * x;
  document.getElementById("squirtleHp").innerHTML = squirtHp - marioCounter;
  squirtHp = squirtHp - marioCounter;
 }
 else if (charPlay == true && squirtDef == true) {
  document.getElementById("squirtleHp").innerHTML = squirtHp - charAtk;
  squirtHp = squirtHp - charAtk;
- y++; charAtk = charAtk * y;
+ y++; charAtk = 40 * y;
  document.getElementById("charmanderHp").innerHTML = charHp - squirtCounter;
  squirtHp = squirtHp - marioCounter;
 }
 else if (charPlay == true && bulbDef == true) {
  document.getElementById("bulbasaurHp").innerHTML = bulbHp - charAtk;
  bulbHp = bulbHp - charAtk;
- y++; charAtk = charAtk * y;
+ y++; charAtk = 40 * y;
  document.getElementById("charmanderHp").innerHTML = charHp - bulbCounter;
  charHp = charHp - bulbCounter;
 }
 else if (charPlay == true && marioDef == true) {
  document.getElementById("marioHp").innerHTML = marioHp - charAtk;
  marioHp = marioHp - charAtk;
- y++; charAtk = charAtk * y;
+ y++; charAtk = 40 * y;
  document.getElementById("charmanderHp").innerHTML = charHp - marioCounter;
  charHp = charHp - marioCounter;
 }
 else if (bulbPlay == true && squirtDef == true) {
  document.getElementById("squirtleHp").innerHTML = squirtHp - bulbAtk;
  squirtHp = squirtHp - bulbAtk;
- z++; bulbAtk = bulbAtk * z;
+ z++; bulbAtk = 5 * z;
  document.getElementById("bulbasaurHp").innerHTML = bulbHp - squirtCounter;
  bulbHp = bulbHp - squirtCounter;
 }
 else if (bulbPlay == true && charDef == true) {
  document.getElementById("charmanderHp").innerHTML = charHp - bulbAtk;
  charHp = charHp - bulbAtk;
- z++; bulbAtk = bulbAtk * z;
+ z++; bulbAtk = 5 * z;
  document.getElementById("bulbasaurHp").innerHTML = bulbHp - charCounter;
  bulbHp = bulbHp - charCounter;
 }
 else if (bulbPlay == true && marioDef == true) {
  document.getElementById("marioHp").innerHTML = marioHp - bulbAtk;
  marioHp = marioHp - bulbAtk;
- z++; bulbAtk = bulbAtk * z;
+ z++; bulbAtk = 5 * z;
  document.getElementById("bulbasaurHp").innerHTML = bulbHp - marioCounter;
  bulbHp = bulbHp - marioCounter;
 }
 else if (marioPlay == true && squirtDef == true) {
  document.getElementById("squirtleHp").innerHTML = squirtHp - marioAtk;
  squirtHp = squirtHp - marioAtk;
- w++; marioAtk = marioAtk * w;
+ w++; marioAtk = 30 * w;
  document.getElementById("marioHp").innerHTML = marioHp - squirtCounter;
  marioHp = marioHp - squirtCounter;
 }
 else if (marioPlay == true && charDef == true) {
  document.getElementById("charmanderHp").innerHTML = charHp - marioAtk;
  charHp = charHp - marioAtk;
- w++; marioAtk = marioAtk * w;
+ w++; marioAtk = 30 * w;
  document.getElementById("marioHp").innerHTML = marioHp - charCounter;
  marioHp = marioHp - charCounter;
 }
 else if (marioPlay == true && bulbDef == true) {
  document.getElementById("bulbasaurHp").innerHTML = bulbHp - marioAtk;
  bulbHp = bulbHp - marioAtk;
- w++; marioAtk = marioAtk * w;
+ w++; marioAtk = 30 * w;
  document.getElementById("marioHp").innerHTML = marioHp - bulbCounter;
  marioHp = marioHp - bulbCounter;
 }
 else if (charPlay == true && mewDef == true) {
  document.getElementById("mewHp").innerHTML = mewHp - charAtk;
  mewHp = mewHp - charAtk;
- y++; charAtk = charAtk * y;
+ y++; charAtk = 40 * y;
  document.getElementById("charmanderHp").innerHTML = charHp - mewCounter;
  charHp = charHp - mewCounter;
 }
 else if (marioPlay == true && mewDef == true) {
  document.getElementById("mewHp").innerHTML = mewHp - marioAtk;
  mewHp = mewHp - marioAtk;
- w++; marioAtk = marioAtk * w;
+ w++; marioAtk = 30 * w;
  document.getElementById("marioHp").innerHTML = marioHp - mewCounter;
  marioHp = marioHp - mewCounter;
 }
 else if (bulbPlay == true && mewDef == true) {
  document.getElementById("mewHp").innerHTML = mewHp - bulbAtk;
  mewHp = mewHp - bulbAtk;
- z++; bulbAtk = bulbAtk * z;
+ z++; bulbAtk = 5 * z;
  document.getElementById("bulbasaurHp").innerHTML = bulbHp - mewCounter;
  bulbHp = bulbHp - mewCounter;
 }
 else if (squirtPlay == true && mewDef == true) {
  document.getElementById("mewtwoHp").innerHTML = mewHp - squirtAtk;
  mewHp = mewHp - squirtAtk;
- x++; squirtAtk = squirtAtk * x;
+ x++; squirtAtk = 25 * x;
  document.getElementById("squirtleHp").innerHTML = squirtHp - mewCounter;
  squirtHp = squirtHp - mewCounter;
 }
@@ -275,15 +275,14 @@ if (bulbHp <= 0) {
 }
 
 
-if (a + b + c + d == 3) {
+if (a + b + c + d >= 3) {
       
       $("#mewtwo").appendTo("#defenderArea");
       mewDef = true;
-     
-  }
-  if (mewHp <= 0) {
-          alert("you beat mewtwo!");
+      if (mewHp <= 0) {
+          alert("you beat mewtwo!")
           document.getElementById("mewtwo").innerHTML = null;
       }
+  }
 
 });
